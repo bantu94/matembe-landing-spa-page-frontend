f383d58510399289742c14c14d0dba9aa01f3a0a
Matto <mattoloma@gmail.com>
2022-12-23 22:30:56 +0300

    landing page portfolio

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#typed', {
+    // Waits 1000ms after typing "First"
+    strings: ['Story Teller', 'Motivater', 'Entertainer', 'Travel Guide'],
+    stringselement: null,
+    typedSpeed: 20,
+    starDelay: 150,
+    backSpeed: 20,
+    backDelay: 2000,
+    loop: true,
+    loopCount: 550,
+    showCursor: true,
+    cursorChar: "|",
+    attr: null,
+    contentTypeL: "html",
+});
